0636861872d45d2722ec06e2767cbffb519f4573
Floh89 <97030217+Floh89@users.noreply.github.com>
2026-08-08 20:25:45 +0200

    Add Palworld 1.0 tower and raid boss catalog

---
+++ data/bosses-local.js
@@ -0,0 +1,17 @@
+window.BOSS_CATALOG={gameVersion:'1.0',updated:'2026-08-08',towers:[
+{id:'zoe-grizzbolt',name:'Zoe & Grizzbolt',pal:'Grizzbolt',tower:'Rayne Syndicate',difficulties:['Normal','Schwer']},
+{id:'lily-lyleen',name:'Lily & Lyleen',pal:'Lyleen',tower:'Free Pal Alliance',difficulties:['Normal','Schwer']},
+{id:'axel-orserk',name:'Axel & Orserk',pal:'Orserk',tower:'Brothers of the Eternal Pyre',difficulties:['Normal','Schwer']},
+{id:'marcus-faleris',name:'Marcus & Faleris',pal:'Faleris',tower:'PIDF',difficulties:['Normal','Schwer']},
+{id:'victor-shadowbeak',name:'Victor & Shadowbeak',pal:'Shadowbeak',tower:'PAL Genetic Research Unit',difficulties:['Normal','Schwer']},
+{id:'saya-selyne',name:'Saya & Selyne',pal:'Selyne',tower:'Moonflowers',difficulties:['Normal','Schwer']},
+{id:'bjorn-bastigor',name:'Bjorn & Bastigor',pal:'Bastigor',tower:'Feybreak',difficulties:['Normal','Schwer']},
+{id:'auri-shaolong',name:'Auri & Shaolong',pal:'Shaolong',tower:'Azure Covenant',difficulties:['Normal','Schwer']},
+{id:'zenara-astralym',name:'Zenara & Astralym',pal:'Astralym',tower:'Nullstar Calamity',difficulties:['Normal','Schwer']}
+],raids:[
+{id:'bellanoir',name:'Bellanoir',pal:'Bellanoir',level:30,difficulties:['Normal'],notes:['Kein Ultra-Datensatz vorhanden.']},
+{id:'bellanoir-libero',name:'Bellanoir Libero',pal:'Bellanoir Libero',level:50,difficulties:['Normal','Ultra']},
+{id:'blazamut-ryu',name:'Blazamut Ryu',pal:'Blazamut Ryu',level:55,difficulties:['Normal','Ultra']},
+{id:'xenolord',name:'Xenolord',pal:'Xenolord',level:60,difficulties:['Normal','Ultra']},
+{id:'hartalis',name:'Hartalis',pal:'Hartalis',level:65,difficulties:['Normal','Ultra']}
+],sources:['Palworld Wiki: Towers','Palworld Wiki: Raid Bosses','Palworld 1.0 patch notes']};
